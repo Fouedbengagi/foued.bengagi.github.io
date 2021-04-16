b1ae316a02e94b45068bfb287da493cc1c13ed66
foued bengagi <foued.ben-gagi-ext@sagemcom.com>
2021-04-16 12:01:09 +0100

    Updates

---
+++ precache-manifest.1f596772af4005c713528cc37a075bdb.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a7ce67ebe0b26f8430da621fe7a1f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e1779abe0ccdf5ae652",
+    "url": "/static/css/3.2ad04f36.chunk.css"
+  },
+  {
+    "revision": "775b558a3013b2dc191f",
+    "url": "/static/css/4.03c720bd.chunk.css"
+  },
+  {
+    "revision": "44993acf329bd9e02d70",
+    "url": "/static/css/main.279806bf.chunk.css"
+  },
+  {
+    "revision": "ab0056531415c1a2d63f",
+    "url": "/static/js/2.f53d5309.chunk.js"
+  },
+  {
+    "revision": "3dbd45c8ed21d092cdcb3d5851d22288",
+    "url": "/static/js/2.f53d5309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1779abe0ccdf5ae652",
+    "url": "/static/js/3.2e640308.chunk.js"
+  },
+  {
+    "revision": "775b558a3013b2dc191f",
+    "url": "/static/js/4.e81e099b.chunk.js"
+  },
+  {
+    "revision": "44993acf329bd9e02d70",
+    "url": "/static/js/main.c933406c.chunk.js"
+  },
+  {
+    "revision": "60bdb9e43a9b4da3d53f",
+    "url": "/static/js/runtime-main.7519c970.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "252ad58d97f15f366d65c99941986515",
+    "url": "/static/media/comp1.252ad58d.jpg"
+  },
+  {
+    "revision": "f0fcb91ad6d99f79b546c765baaf8122",
+    "url": "/static/media/comp2.f0fcb91a.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "44e8cd31e04ada138dac5d70e9e5abf1",
+    "url": "/static/media/isamm.44e8cd31.png"
+  },
+  {
+    "revision": "45d34795199f167b2366e15294d3b118",
+    "url": "/static/media/logo-talan.45d34795.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "283108aab60ac64cecdbed2603c45514",
+    "url": "/static/media/siconia.283108aa.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
