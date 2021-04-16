831f672168a752a2f31ed4bfdd7743ed32fcf863
foued bengagi <foued.ben-gagi-ext@sagemcom.com>
2021-04-16 12:55:39 +0100

    Updates

---
+++ precache-manifest.d0caaffa4091e93ecd460e4adb393dcf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0a7dda152601d94895a0455837348b",
+    "url": "/foued.bengagi.github.io/index.html"
+  },
+  {
+    "revision": "9e1779abe0ccdf5ae652",
+    "url": "/foued.bengagi.github.io/static/css/3.2ad04f36.chunk.css"
+  },
+  {
+    "revision": "775b558a3013b2dc191f",
+    "url": "/foued.bengagi.github.io/static/css/4.03c720bd.chunk.css"
+  },
+  {
+    "revision": "3af3b26bcbda0caa4bfe",
+    "url": "/foued.bengagi.github.io/static/css/main.72abf958.chunk.css"
+  },
+  {
+    "revision": "ab0056531415c1a2d63f",
+    "url": "/foued.bengagi.github.io/static/js/2.f53d5309.chunk.js"
+  },
+  {
+    "revision": "3dbd45c8ed21d092cdcb3d5851d22288",
+    "url": "/foued.bengagi.github.io/static/js/2.f53d5309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1779abe0ccdf5ae652",
+    "url": "/foued.bengagi.github.io/static/js/3.2e640308.chunk.js"
+  },
+  {
+    "revision": "775b558a3013b2dc191f",
+    "url": "/foued.bengagi.github.io/static/js/4.e81e099b.chunk.js"
+  },
+  {
+    "revision": "3af3b26bcbda0caa4bfe",
+    "url": "/foued.bengagi.github.io/static/js/main.d04cb5fa.chunk.js"
+  },
+  {
+    "revision": "0e61ea1d11579a2fbffa",
+    "url": "/foued.bengagi.github.io/static/js/runtime-main.e0de07e1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/foued.bengagi.github.io/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/foued.bengagi.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/foued.bengagi.github.io/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "252ad58d97f15f366d65c99941986515",
+    "url": "/foued.bengagi.github.io/static/media/comp1.252ad58d.jpg"
+  },
+  {
+    "revision": "f0fcb91ad6d99f79b546c765baaf8122",
+    "url": "/foued.bengagi.github.io/static/media/comp2.f0fcb91a.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/foued.bengagi.github.io/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/foued.bengagi.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "44e8cd31e04ada138dac5d70e9e5abf1",
+    "url": "/foued.bengagi.github.io/static/media/isamm.44e8cd31.png"
+  },
+  {
+    "revision": "45d34795199f167b2366e15294d3b118",
+    "url": "/foued.bengagi.github.io/static/media/logo-talan.45d34795.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/foued.bengagi.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/foued.bengagi.github.io/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "283108aab60ac64cecdbed2603c45514",
+    "url": "/foued.bengagi.github.io/static/media/siconia.283108aa.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/foued.bengagi.github.io/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/foued.bengagi.github.io/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
